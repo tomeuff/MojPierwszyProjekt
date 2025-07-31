0b799623e8e60c18a021966b8f8cfc0cfbdb771e
tomeuff <tomaszkraczaj1992@gmail.com>
2025-08-01 00:24:46 +0200

    Dodanie funkcji NWD_lista()

---
+++ działania_na_liczbach_calkowitych.py
@@ -0,0 +1,13 @@
+def NWD_lista(lst):
+    if not lst:
+        return None
+    wynik = abs(lst[0])
+    for liczba in lst[1:]:
+        a, b = abs(wynik), abs(liczba)
+        while a != b:
+            if a > b:
+                a -= b
+            else:
+                b -= a
+        wynik = a
+    return wynik
